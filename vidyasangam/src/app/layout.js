798f980426c125927c413a967ff80257184f056c
Makarand-Tighare <makarand.tighare@gmail.com>
2024-10-11 20:22:30 +0530

    added favicon ico

--- vidyasangam/src/app/layout.js
+++ vidyasangam/src/app/layout.js
@@ -26,16 +26,17 @@ const roboto = Roboto({
 // Metadata for the Vidyasangam platform
 export const metadata = {
   title: "VidyaSangam",
-  description: "Empowering students to bridge the gap between academia and industry.",
+  description:
+    "Empowering students to bridge the gap between academia and industry.",
   icons: {
     icon: [
       {
-        url: './vidyasangam-logo.png',
-        media: '(prefers-color-scheme: light)',
+        url: "./vidyasangam-logo.png",
+        media: "(prefers-color-scheme: light)",
       },
       {
-        url: './vidyasangam-logo.png',
-        media: '(prefers-color-scheme: dark)',
+        url: "./vidyasangam-logo.png",
+        media: "(prefers-color-scheme: dark)",
       },
     ],
   },
@@ -45,9 +46,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       {/* <body className={`${poppins.variable} ${comfortaa.variable} ${roboto.variable} bg-gradient-to-r from-orange-50 from-10% via-violet-100 via-30% mb-0 pb-0`}> */}
-      <body className={`${poppins.variable} ${comfortaa.variable} ${roboto.variable} bg-gradient-to-r from-[#e6f3ff] via-[#f0f8ff] to-[#f5faff] h-full mb-0 pb-0`}>
+      <body
+        className={`${poppins.variable} ${comfortaa.variable} ${roboto.variable} bg-gradient-to-r from-[#e6f3ff] via-[#f0f8ff] to-[#f5faff] h-full mb-0 pb-0`}
+      >
         {children}
       </body>
     </html>
